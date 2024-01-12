00a6cbe77a631c690488a99c74064fc8af6b2ca1
Andrea Maldonado <>
2024-01-12 14:33:38 +0100

    WIP: Moves simple stats to own class

--- feeed/simple_stats.py
+++ feeed/simple_stats.py
@@ -1,18 +1,32 @@
 from pm4py.algo.filtering.log.variants import variants_filter
 
+class SimpleStats:
+    def __init__(self, log):
+        self.log = log
+        #print("INIT", self.n_traces(self.log))
+
+    @classmethod
+    def n_traces(self, log):
+        return len(log)
+
+    @classmethod
+    def n_unique_traces(self, log):
+        variants = variants_filter.get_variants(log)
+        return len(variants)
+
+    @classmethod
+    def ratio_unique_traces_per_trace(self, log):
+        variants = variants_filter.get_variants(log)
+
 
 def simple_stats(log):
-    n_traces = len(log)
-
-    variants = variants_filter.get_variants(log)
-    n_unique_traces = len(variants)
-
-    ratio_unique_traces_per_trace = n_unique_traces / n_traces
+    simplestats = SimpleStats(log)
+    print("TEST")
 
     result = {
-            "n_traces": n_traces,
-            "n_unique_traces": n_unique_traces,
-            "ratio_unique_traces_per_trace": ratio_unique_traces_per_trace
+            "n_traces": simplestats.n_traces(log),
+            "n_unique_traces": simplestats.n_unique_traces(log),
+            "ratio_unique_traces_per_trace": simplestats.n_unique_traces(log)/ simplestats.n_traces(log)
             }
 
     return result

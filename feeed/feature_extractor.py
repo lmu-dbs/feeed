7d29161952ef737e626350056c42e9b008e87e00
Andrea Maldonado <>
2024-01-15 18:45:46 +0100

    Form constant to DRY

--- feeed/feature_extractor.py
+++ feeed/feature_extractor.py
@@ -11,34 +11,28 @@ from .time import TimeBased as time_based
 from datetime import datetime as dt
 from pm4py.objects.log.importer.xes import importer as xes_importer
 
+FEATURE_TYPES = [
+    "simple_stats",
+    "trace_length",
+    "trace_variant",
+    "activities",
+    "start_activities",
+    "end_activities",
+    "entropies",
+    "complexity",
+    "time_based",
+    ]
+
+
 def feature_type(feature_name):
-    feature_types = [
-            "simple_stats",
-            "trace_length",
-            "trace_variant",
-            "activities",
-            "start_activities",
-            "end_activities",
-            "entropies",
-            "complexity",
-            "time_based",
-        ]
-
-    single_selection_features = ['simple_stats', 'trace_length','trace_variant',
-                                 'activities', 'start_activities', 'end_activities',
-                                 'entropies', 'complexity', 'time_based']
-
-    for feature_type in feature_types:
-        if feature_type in single_selection_features:
-            available_features = [*eval(feature_type)().available_class_methods]
-        else:
-            available_features = []
+    available_features = []
+    for feature_type in FEATURE_TYPES:
+        available_features.extend([*eval(feature_type)().available_class_methods])
         available_features.append(str(feature_type))
         if feature_name in available_features:
             return feature_type
-    raise ValueError(f"ERROR: Invalid value for feature_key argument: {feature_name}. See README.md for supported feature_names or  Use a sublist of the following:"
-            "\n['simple_stats', 'trace_length', 'trace_variant', 'activities', 'start_activities', 'end_activities',",
-            "\n'entropies', 'complexity', 'time_based] or None")
+    raise ValueError(f"ERROR: Invalid value for feature_key argument: {feature_name}. See README.md for " +
+                     f"supported feature_names or use a sublist of the following: {FEATURE_TYPES} or None")
 
 def extract_features(event_logs_path, feature_types=None):
     log_name = event_logs_path.rsplit("/", 1)[-1]
@@ -47,17 +41,7 @@ def extract_features(event_logs_path, feature_types=None):
     )
 
     if feature_types is None:
-        feature_types = [
-            "simple_stats",
-            "trace_length",
-            "trace_variant",
-            "activities",
-            "start_activities",
-            "end_activities",
-            "entropies",
-            "complexity",
-            "time_based",
-        ]
+        feature_types = FEATURE_TYPES
 
     features = {"log": log_name.split(".xes")[0]}
     start_log = dt.now()
@@ -78,6 +62,6 @@ def extract_features(event_logs_path, feature_types=None):
         else:
             print(log_info + f"next {feature_types[(i+1)%len(feature_types)]}...")
     print(
-        f"SUCCESSFULLY: {len(features)} features for {log_name} took {dt.now() - start_log} sec."
+        f"SUCCESSFULLY: {len(features)-1} features for {log_name} took {dt.now() - start_log} sec."
     )
     return features

adba0c1361fae1217fefb9fb3fe3334423c583f5
Andrea Maldonado <23620851+andreamalhera@users.noreply.github.com>
2026-09-10 19:12:50 +0200

    Bump version fallback to 2.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version_string = os.environ.get("VERSION_PLACEHOLDER", "2.0.0")
+version_string = os.environ.get("VERSION_PLACEHOLDER", "2.1.0")
 print(version_string)
 version = version_string
 
